1092b23c7039d730d13a7b2a58ca42e7089902aa
Spreadyy <sandro@012345.ch>
2016-06-16 22:38:51 +0200

    first commit

---
+++ js/site.js
@@ -0,0 +1,30 @@
+﻿// Write your Javascript code.
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        updateNavigationFixedWhenDesktop();
+    });
+
+    $(window).resize(function () {
+        updateNavigationFixedWhenDesktop();
+    });
+
+    function updateNavigationFixedWhenDesktop() {
+        if ($(window).width() > 820) {
+            updateNavigationFixed();
+        }
+    }
+
+    function updateNavigationFixed() {
+        var headerHeight = $("header .headerBackground").outerHeight();
+
+        if ($(window).scrollTop() > headerHeight && !$("header nav").hasClass("fixed")) {
+            $("header nav").addClass("fixed");
+        }
+        else if ($(window).scrollTop() <= headerHeight && $("header nav").hasClass("fixed")) {
+            $("header nav").removeClass("fixed");
+        }
+    }
+
+});
